44d8ac3605736b3b69e13e602a8076c932aa0f3b
francisco-ag <francisamado80@gmail.com>
2024-01-09 10:37:00 -0600

    Primer commit

---
+++ tiposEstructurasDatos.py
@@ -0,0 +1,16 @@
+
+#Estructura Lineal Homogenea
+estructuraLineal = [2,4,5,7]
+print (estructuraLineal)
+print ("El array tiene un tamaño",len(estructuraLineal))
+
+#Estructuras Lineales Heterogeneas
+estructuraHeterogenea = [1,"1",'hola',True,5.2]
+print (estructuraHeterogenea[2])
+print ("El array Heterogeneo tiene un tamaño",len(estructuraHeterogenea))
+
+
+
+
+
+
