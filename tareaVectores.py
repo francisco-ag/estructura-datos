cc58682dcb142e9b3ce40784bbd85599f4b78b49
francisco-ag <francisamado80@gmail.com>
2024-01-11 10:31:30 -0600

    tarea

---
+++ tareaVectores.py
@@ -0,0 +1,14 @@
+# Crear un programa que almacene nombres en vector
+# capturará datos hasta que el valor ingresado, se 'STOP'
+# mostrar el vector resultante
+# (Sugerencia: usar metodo while )
+
+nombres = []
+
+while True:
+    nombre = str(input("Ingresa un nombre: "))
+    if nombre == "STOP":
+        break
+    else:
+        nombres.append(nombre)
+print("Nombres almacenados: ", nombres)
