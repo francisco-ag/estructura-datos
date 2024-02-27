22c23f29a2aab999172f30733c6e0a47fd87c8c3
francisco-ag <francisamado80@gmail.com>
2024-02-27 10:55:27 -0600

    CR

--- EstructurasLineales/ejemploProyecto/Restaurante.py
+++ EstructurasLineales/ejemploProyecto/Restaurante.py
@@ -7,7 +7,7 @@ class NodoListaEnlazada():
     def __init__(self, dato):
         self.dato = dato
         self.siguiente = None
-        print(dato)
+
 
 
 class ListaEnlazada():
@@ -15,7 +15,7 @@ class ListaEnlazada():
         self.cabeza = None
 
     def agregar_al_principio(self, dato):
-        print(dato)
+        
         nuevo_nodo = NodoListaEnlazada(dato)
         nuevo_nodo.siguiente = self.cabeza
         self.cabeza = nuevo_nodo
@@ -33,12 +33,18 @@ class ListaEnlazada():
                 self.cabeza = actual.siguiente
     
     def mostrar_lista(self):
-        
-        actual = self.cabeza
-        
+        actual = self.cabeza    
         while actual:
             print(actual.dato)
             actual = actual.siguiente
+    
+    def obtener_lista(self):
+        lista = []
+        actual = self.cabeza
+        while actual:
+            lista.append(actual.dato)
+            actual = actual.siguiente
+        return lista
 
 class RestauranteApp(tk.Tk):
     def __init__(self):
@@ -94,6 +100,11 @@ class RestauranteApp(tk.Tk):
         btn_agregar = tk.Button(frame_empleados, text="Agregar Empleado", command=self.agregar_empleado)
         btn_agregar.grid(row=1, column=0, columnspan=2, padx=5, pady=5)
 
+         # Crear un Listbox para mostrar los empleados
+        self.listbox_empleados = tk.Listbox(frame_empleados)
+        self.listbox_empleados.grid(row=2, column=0, columnspan=2, padx=5, pady=5, sticky="nsew")
+
+
     def agregar_empleado(self):
         # Obtener el nombre del empleado ingresado en la entrada
         nombre_empleado = self.entry_nombre.get()
@@ -101,21 +112,39 @@ class RestauranteApp(tk.Tk):
         # Verificar si se ingresó un nombre
         if nombre_empleado:
             # Crear un nuevo nodo con el nombre del empleado
-            nuevo_empleado = NodoListaEnlazada(nombre_empleado)
-            #print(nombre_empleado)
+            #nuevo_empleado = NodoListaEnlazada(nombre_empleado)
+            
             # Agregar el nuevo empleado a la lista enlazada de empleados
-            self.lista_empleados.agregar_al_principio(nuevo_empleado)
+            self.lista_empleados.agregar_al_principio(nombre_empleado)
 
             # Actualizar la visualización de la lista de empleados
-            #self.lista_empleados.actualizar_lista_empleados()
+            self.actualizar_lista_empleados()
+
 
             # Limpiar la entrada después de agregar el empleado
             self.entry_nombre.delete(0, tk.END)
 
             self.lista_empleados.mostrar_lista()
+
+            empleados = []
+            empleados.append(self.lista_empleados.mostrar_lista())
+
+            messagebox.showinfo("Infor","Guardado exitosamente")
+
+
         else:
             # Mostrar un mensaje de error si no se ingresó un nombre
             messagebox.showerror("Error", "Por favor ingresa el nombre del empleado.")
+
+    def actualizar_lista_empleados(self):
+        # Limpiar el contenido actual del Listbox
+        self.listbox_empleados.delete(0, tk.END)
+        
+        # Obtener la lista de empleados y agregarlos al Listbox
+        empleados = self.lista_empleados.obtener_lista()
+        for empleado in empleados:
+            self.listbox_empleados.insert(tk.END, empleado)
+
 
     def create_turnos_widgets(self):
         # Aquí crearías los widgets para la pestaña de turnos
